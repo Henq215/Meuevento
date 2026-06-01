7fa84a2c8649d5ee51dfc32cdcbb49874670f5cc
Henq215 <109037390+Henq215@users.noreply.github.com>
2026-06-01 10:43:29 -0300

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,26 @@
+﻿using Meuevento.Models;
+
+namespace Meuevento
+{
+    public partial class App : Application
+    {
+        
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new Views.Inicial());
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var window = base.CreateWindow(activationState);
+
+            window.Width = 400;
+            window.Height = 600;
+
+            return window;
+        }
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Meuevento")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Meuevento.Pages")]
